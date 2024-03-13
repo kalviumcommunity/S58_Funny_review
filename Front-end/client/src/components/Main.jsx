f8af5abfda5d671779b8b35e41e5c0e153d482cb
unknown <khushi.nain@kalvium.community>
2024-03-13 15:44:15 +0530

    displaye dummy data

--- Front-end/client/src/components/Main.jsx
+++ Front-end/client/src/components/Main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
 export default function Main() {
+  
   return (
     <div>
-        <h1>This is the Landing Page</h1>
+      Sr_No: 1,<br></br>
+    Name: 'Pizza Freakers',<br></br>
+    img_url: "https://source.unsplash.com/random/200x200?sig=3",<br></br>
+    Ratings: 4,<br></br>
+    Review: "Sandwiches packed to perfection. Sauce dripping, napkins flying",<br></br>
+    Location: 'Chandigarh'<br></br>
+    
     </div>
-  )
+  ) 
 }
