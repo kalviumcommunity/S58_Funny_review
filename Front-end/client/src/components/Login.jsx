15b13ff40235835d6694b2ac0d653d22ae172e5c
unknown <khushi.nain@kalvium.community>
2024-03-20 17:24:55 +0530

    adding data to db

---
+++ Front-end/client/src/components/Login.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import {useEffect} from 'react';
+import axios from 'axios';
+
+export default function Login() {
+
+  const [logInfo, setLogInfo]= useState({
+    username:"",
+    password:""
+  })
+
+  const [data,setData]= useState([])
+
+  useEffect(()=>{
+    axios.get('http://localhost:7777/routes/getusers')
+    .then((user)=>{setData(user.data);
+    console.log(user.data)})
+    .catch((error)=>{console.log(error)})
+  },[])
+
+  
+  const handleChange=(e,field)=>{
+    e.preventDefault(); 
+    if (field=="username"){
+      setLogInfo({...logInfo,username:e.target.value})
+    }
+    else{
+      setLogInfo({...logInfo, password : e.target.value})
+    }
+  }
+  
+
+  const handleSubmit=(e)=>{
+    e.preventDefault(); 
+    axios.post('http://localhost:7777/routes/adduser', logInfo)
+    .then((response) => {
+        console.log(response.data);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+  }
+
+  return (
+    <>
+        <form action="" onSubmit={(e)=>handleSubmit(e)}>
+        <label> Enter your UserName <br />
+          <input onChange={(e)=>handleChange(e,"username")} name='username' type="text" placeholder='UserName'/>
+        </label><br></br>
+        <label htmlFor="">Enter your password <br />
+          <input onChange={(e)=>handleChange(e,"password")} type="password" placeholder='Password' />
+        </label><br></br>
+        <label htmlFor="">Confirm your password <br />
+          <input type="password" placeholder='Repeat password'/>
+        </label>
+        <br />
+        <button type='submit' >SIGNUP</button>
+      </form>
+      <div>
+        <table>
+      {
+      data.map(user=>{
+        return <tr>
+            <td>username: {user.username}</td>
+            <td>password: {user.password}</td>
+          </tr>
+      })
+      }
+      </table>
+    </div>
+
+    
+    </>
+   
+  )
+}
