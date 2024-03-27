75f27d16d832d40aefc85e81f7dce18a602d6f93
unknown <khushi.nain@kalvium.community>
2024-03-27 14:31:09 +0530

    validate

--- Front-end/client/src/components/Login.jsx
+++ Front-end/client/src/components/Login.jsx
@@ -19,7 +19,7 @@ export default function Login() {
   },[])
 
   
-  const handleChange=(e,field)=>{
+  const handleSignUpChange=(e,field)=>{
     e.preventDefault(); 
     if (field=="username"){
       setSignInfo({...signInfo,username:e.target.value})
@@ -30,7 +30,7 @@ export default function Login() {
   }
   
 
-  const handleSubmit=(e)=>{
+  const handleSignUpSubmit=(e)=>{
     e.preventDefault(); 
     axios.post('http://localhost:7777/signUp/', signInfo)
     .then((response) => {
@@ -43,19 +43,33 @@ export default function Login() {
 
   return (
     <>
-        <form action="" onSubmit={(e)=>handleSubmit(e)}>
-        <label> Enter your UserName <br />
-          <input onChange={(e)=>handleChange(e,"username")} name='username' type="text" placeholder='UserName'/>
-        </label><br></br>
-        <label htmlFor="">Enter your password <br />
-          <input onChange={(e)=>handleChange(e,"password")} type="password" placeholder='Password' />
-        </label><br></br>
-        <label htmlFor="">Confirm your password <br />
-          <input type="password" placeholder='Repeat password'/>
-        </label>
-        <br />
-        <button type='submit' >SIGNUP</button>
-      </form>    
+      <div>
+        <form action="" onSubmit={(e)=>handleSignUpSubmit(e)}>
+          <label> Enter your UserName <br />
+            <input onChange={(e)=>handleSignUpChange(e,"username")} name='username' type="text" placeholder='UserName'/>
+          </label><br></br>
+          <label htmlFor="">Create your password <br />
+            <input onChange={(e)=>handleSignUpChange(e,"password")} type="password" placeholder='Password' />
+          </label><br></br>
+          <label htmlFor="">Confirm your password <br />
+            <input type="password" placeholder='Repeat password'/>
+          </label>
+          <br />
+          <button type='submit' >SIGNUP</button>
+        </form>    
+      </div>
+      <div>
+      <form action="" onSubmit={(e)=>handleLogInSubmit(e)}>
+          <label> Enter your UserName <br />
+            <input onChange={(e)=>handleLogInChange(e,"username")} name='username' type="text" placeholder='UserName'/>
+          </label><br></br>
+          <label htmlFor="">Enter your password <br />
+            <input onChange={(e)=>handleLogInChange(e,"password")} type="password" placeholder='Password' />
+          </label><br></br>
+          <button type='submit' >LOGIN</button>
+        </form> 
+
+      </div>
     </>
    
   )
